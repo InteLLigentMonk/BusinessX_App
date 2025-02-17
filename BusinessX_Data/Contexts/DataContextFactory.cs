45921f0f4265dd1d1a55f267b98dd396c945e5f7
Tobias Vinther <Tobias.Vinther88@gmail.com>
2025-02-17 07:47:06 +0100

    Small update

--- BusinessX_Data/Contexts/DataContextFactory.cs
+++ BusinessX_Data/Contexts/DataContextFactory.cs
@@ -8,7 +8,7 @@ public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
     public DataContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
-        optionsBuilder.UseNpgsql("Host=192.168.1.200;Port=5432;Database=BusinessX;Username=InteLLi;Password=Lv2+Fy2t");
+        optionsBuilder.UseNpgsql("Host=192.168.1.200;Port=5432;Database=BusinessX;Username=*****;Password=*******");
 
         return new DataContext(optionsBuilder.Options);
     }
